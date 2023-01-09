922afa7aa73f952520a171badd2bf27baca3c6f1
squidward tentacles <pappasgavriel@gmail.com>
2023-01-09 14:26:04 -0600

    change motor controllers for testing

--- src/main/java/frc/robot/subsystems/DriveData.java
+++ src/main/java/frc/robot/subsystems/DriveData.java
@@ -1,21 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
 public class DriveData {
 
-    public final PWMSparkMax frontLeftDrive = new PWMSparkMax(Constants.Drive.FRONT_LEFT_PORT);
-    public final PWMSparkMax frontRightDrive = new PWMSparkMax(Constants.Drive.FRONT_RIGHT_PORT);
-    public final PWMSparkMax backLeftDrive = new PWMSparkMax(Constants.Drive.BACK_LEFT_PORT);
-    public final PWMSparkMax backRightDrive = new PWMSparkMax(Constants.Drive.BACK_RIGHT_PORT);
+    public final WPI_VictorSPX frontLeftDrive = new WPI_VictorSPX(Constants.Drive.FRONT_LEFT_PORT);
+    public final WPI_VictorSPX frontRightDrive = new WPI_VictorSPX(Constants.Drive.FRONT_RIGHT_PORT);
+    public final WPI_VictorSPX backLeftDrive = new WPI_VictorSPX(Constants.Drive.BACK_LEFT_PORT);
+    public final WPI_VictorSPX backRightDrive = new WPI_VictorSPX(Constants.Drive.BACK_RIGHT_PORT);
 
     public DriveData() {
         this.frontLeftDrive.setInverted(Constants.Drive.FRONT_LEFT_FLIPPED);
         this.frontRightDrive.setInverted(Constants.Drive.FRONT_RIGHT_FLIPPED);
         this.backLeftDrive.setInverted(Constants.Drive.BACK_LEFT_FLIPPED);
         this.backRightDrive.setInverted(Constants.Drive.BACK_RIGHT_FLIPPED);
+
     }
 
     public void sendTelemetry() {
