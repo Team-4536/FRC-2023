a5ed58b00c407c8d24649a04a871334e813430fb
robbie aldrich <robbiemaldrich@gmail.com>
2023-01-09 22:04:40 -0600

    missed a thing from bot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,7 +29,11 @@ public class Robot extends TimedRobot {
 
     // runs once when the robot is turned on
     @Override
-    public void robotInit() { new Constants().load(); Constants.ROBOT_INIT_FUNC.accept(this); }
+    public void robotInit() {
+        new Constants().load();
+        Constants.ROBOT_INIT_FUNC.accept(this);
+        TelemetryUtil.clearDashboard();
+    }
 
     // runs constantly, no matter the mode
     // don't put motor control stuff in here lol
