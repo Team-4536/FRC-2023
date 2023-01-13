c01460ef4401cbf44808284f0ec07ac089eebdda
Robbie <robbiemaldrich@gmail.com>
2023-01-13 11:00:25 -0600

    config goes poof program is better

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,9 +4,9 @@
 
 package frc.robot;
 
+import java.util.function.Consumer;
 
-
-
+import frc.robot.behaviours.TestingBehaviour;
 
 public final class Constants {
 
@@ -17,5 +17,45 @@ public final class Constants {
 
     public static final double POWER_SCALE_UPPER_BOUND = 1.0;
     public static final double POWER_SCALE_LOWER_BOUND = 0.2;
+
+
+
+
+    public static final int DRIVE_FRONT_LEFT_PORT = 2;
+    public static final int DRIVE_FRONT_RIGHT_PORT = 1;
+    public static final int DRIVE_BACK_LEFT_PORT = 3;
+    public static final int DRIVE_BACK_RIGHT_PORT = 4;
+
+    public static final boolean DRIVE_FRONT_LEFT_FLIPPED = true;
+    public static final boolean DRIVE_FRONT_RIGHT_FLIPPED = false;
+    public static final boolean DRIVE_BACK_LEFT_FLIPPED = true;
+    public static final boolean DRIVE_BACK_RIGHT_FLIPPED = false;
+
+
+
+    public static final int CONTROLLER_PORT = 0;
+    public static final int JOYSTICK_PORT = 1;
+
+
+
+
+
+
+
+
+    private static final Consumer<Robot> NULL_FUNC = new Consumer<Robot>() {
+        @Override public void accept(Robot r) { } };
+
+    public static final Consumer<Robot> ROBOT_INIT_FUNC = NULL_FUNC;
+    public static final Consumer<Robot> ROBOT_PER_FUNC = TestingBehaviour.botPeriodic;
+
+    public static final Consumer<Robot> TELEOP_INIT_FUNC = NULL_FUNC;
+    public static final Consumer<Robot> TELEOP_PER_FUNC = TestingBehaviour.teleOpPeriodic;
+
+    public static final Consumer<Robot> AUTO_INIT_FUNC = NULL_FUNC;
+    public static final Consumer<Robot> AUTO_PER_FUNC = NULL_FUNC;
+
+    public static final Consumer<Robot> DISABLED_INIT_FUNC = TestingBehaviour.disabledInit;
+    public static final Consumer<Robot> DISABLED_PER_FUNC = TestingBehaviour.disabledPeriodic;
 
 }
--- src/main/java/frc/robot/subsystems/InputData.java
+++ src/main/java/frc/robot/subsystems/InputData.java
@@ -4,8 +4,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Configured;
 import frc.robot.functions.TelemetryUtil;
+import frc.robot.Constants;
 
 public final class InputData {
 
@@ -15,8 +15,8 @@ public final class InputData {
 
 
     public InputData() {
-        controller = new XboxController(Configured.CONTROLLER_PORT);
-        joystick = new Joystick(Configured.JOYSTICK_PORT);
+        controller = new XboxController(Constants.CONTROLLER_PORT);
+        joystick = new Joystick(Constants.JOYSTICK_PORT);
     }
 
 
