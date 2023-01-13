ce96886153e0b41a29a6a60ca0347164974aad32
Robbie <robbiemaldrich@gmail.com>
2023-01-13 16:44:01 -0600

    test ready

--- src/main/java/frc/robot/behaviours/TestingBehaviour.java
+++ src/main/java/frc/robot/behaviours/TestingBehaviour.java
@@ -2,6 +2,7 @@ package frc.robot.behaviours;
 
 import java.util.function.Consumer;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.functions.driveUtil;
 import frc.robot.functions.telemetryUtil;
@@ -23,6 +24,18 @@ public class TestingBehaviour {
 
             driveUtil.setPowerTank(r.drive, r.input.controller.getLeftY(), r.input.controller.getLeftX(), r.input.controller.getRightTriggerAxis());
             telemetryUtil.grabChoosers();
+        }
+    };
+
+    public static Consumer<Robot> auto_1 = new Consumer<Robot>() {
+        @Override public void accept(Robot r) {
+            SmartDashboard.putString("auto func", "auto 1");
+        }
+    };
+
+    public static Consumer<Robot> auto_2 = new Consumer<Robot>() {
+        @Override public void accept(Robot r) {
+            SmartDashboard.putString("auto func", "auto 2");
         }
     };
 }
--- src/main/java/frc/robot/functions/telemetryUtil.java
+++ src/main/java/frc/robot/functions/telemetryUtil.java
@@ -1,11 +1,15 @@
 package frc.robot.functions;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 
-import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 
 public class telemetryUtil {
 
@@ -43,12 +47,56 @@ public class telemetryUtil {
 
     public static void initChoosers() {
 
+
+        ArrayList<String> funcNames = new ArrayList<String>(List.of(
+            "BehaviourUtil.stopDrive",
+            "FinalBehaviour.robotInit",
+            "FinalBehaviour.teleOpPeriodic",
+            "TestingBehaviour.init",
+            "TestingBehaviour.periodic",
+            "TestingBehaviour.auto_1",
+            "TestingBehaviour.auto_2"
+        ));
+
+
+        AutoInitPicker = new SendableChooser<String>();
+        SmartDashboard.putData(AutoInitPicker);
+        AutoPerPicker = new SendableChooser<String>();
+        SmartDashboard.putData(AutoPerPicker);
+
         AutoInitPicker.setDefaultOption("nothing", "");
         AutoPerPicker.setDefaultOption("nothing", "");
+
+        for(int i = 0; i < funcNames.size(); i++) {
+            String s = funcNames.get(i);
+            AutoInitPicker.addOption(s, s);
+            AutoPerPicker.addOption(s, s);
+        }
+
     }
 
     public static void grabChoosers() {
+        try {
 
+            String s = AutoInitPicker.getSelected();
+            Class<?> c = Class.forName(s.substring(0,s.indexOf('.')));
+            Field f = c.getField(s.substring(s.indexOf('.')+1));
+            Object o = f.get(null);
+            if(o instanceof Consumer<?>) {
+                Robot.AUTO_INIT_FUNC = (Consumer<Robot>)o; }
+
+            s = AutoPerPicker.getSelected();
+            c = Class.forName(s.substring(0,s.indexOf('.')));
+            f = c.getField(s.substring(s.indexOf('.')+1));
+            o = f.get(null);
+            if(o instanceof Consumer<?>) {
+                Robot.AUTO_PER_FUNC = (Consumer<Robot>)o; }
+
+
+
+        } catch(Exception e) {
+            telemetryUtil.logError("getting choosers failed: " + e.toString());
+        }
     }
 
 
