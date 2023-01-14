8db6365501577827e6e4396099e74d1cd59cdc16
Robbie <robbiemaldrich@gmail.com>
2023-01-14 11:16:04 -0600

    untested move distance

--- src/main/java/frc/robot/behaviours/AuoFunctions.java
+++ src/main/java/frc/robot/behaviours/AuoFunctions.java
@@ -2,11 +2,22 @@ package frc.robot.behaviours;
 
 import java.util.function.Consumer;
 
+import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.functions.driveUtil;
 
 public class AuoFunctions {
 
-    public static Consumer<Robot> autoTest = r -> {
+    public static Consumer<Robot> autoTest = robot -> {
+    
+        int backLeftE = robot.drive.backLeftEncoder.get();
 
+
+
+        if(backLeftE >= Constants.TICKS_PER_INCH * 48){
+            driveUtil.setPowerUniform(robot.drive, 0);
+        } else{
+            driveUtil.setPowerUniform(robot.drive, 0.1);
+        }
     };
 }
