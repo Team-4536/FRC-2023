b6d26f2813dbf10af1209195009e2fdb12eadb15
robbie aldrich <robbiemaldrich@gmail.com>
2023-01-14 23:24:16 -0600

    stash

--- src/main/java/frc/robot/behaviours/TestingBehaviour.java
+++ src/main/java/frc/robot/behaviours/TestingBehaviour.java
@@ -3,19 +3,16 @@ package frc.robot.behaviours;
 import java.util.function.Consumer;
 
 import frc.robot.Robot;
-import frc.robot.controllers.PIDController;
 import frc.robot.functions.driveUtil;
 import frc.robot.functions.telemetryUtil;
 
 public class TestingBehaviour {
 
-    static PIDController controller = new PIDController();
-    static double vel = 0;
-
 
     public static Consumer<Robot> periodic = r -> {
 
         double d = r.drive.pidController.tick(r.gyro.globGyroscope.getAngle(), Robot.dt, true);
+        telemetryUtil.debugLog(String.valueOf(r.drive.pidController.integral));
         driveUtil.setPowerTank(r.drive, r.input.controller.getLeftY(), d, r.input.controller.getRightTriggerAxis()*2 - 1);
 
     };
